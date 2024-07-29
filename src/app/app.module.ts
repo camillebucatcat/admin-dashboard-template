bd05bc860d053a7fe2511ebfa6b090897c328258
Camille Bucatcat <camille.bucatcat@gmail.com>
2024-07-29 22:43:40 +0800

    init files

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterOutlet } from '@angular/router'
+import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzConfig, NZ_CONFIG } from 'ng-zorro-antd/core/config';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+registerLocaleData(en);
+
+const ngZorroConfig: NzConfig = {
+  theme: {
+    // primaryColor: '#1DA57A'
+  }
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NzLayoutModule,
+    NzButtonModule,
+    NzBreadCrumbModule,
+    NzMenuModule
+  ],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+    { provide: NZ_CONFIG, useValue: ngZorroConfig },
+    provideAnimationsAsync(),
+    provideHttpClient()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
